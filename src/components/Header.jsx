12b76874bffed35d9725ecc6825fcf8519b8e301
Shruti Kodinariya <98315807+ShrutiKodinariya@users.noreply.github.com>
2022-09-09 11:12:03 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
-
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../firebase.config";
-
 import Logo from "../img/headerlogo.svg";
 import Logo1 from "../img/hl1.svg";
 import Avatar from "../img/avatar.png";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
+import { Nav, Navbar, NavbarBrand } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
@@ -53,33 +54,49 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-black">
+    <Navbar className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 aaf-nav">
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={"/"} className="flex items-center gap-2">
+        <Navbar.Brand to={"/"} className="flex items-center gap-2">
           <img src={Logo1} className="object-cover" alt="logo1" />
           <img src={Logo} className="object-cover" alt="logo" />
-        </Link>
+        </Navbar.Brand>
 
-        <div className="flex items-center gap-8">
+        <Nav className="flex items-center gap-8">
           <motion.ul
             initial={{ opacity: 0, x: 200 }}
             animate={{ opacity: 1, x: 0 }}
             exit={{ opacity: 0, x: 200 }}
             className="flex items-center gap-24 "
           >
-            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            <Nav.Link
+              as={Link}
+              to={"/"}
+              className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Home
-            </li>
-            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/ourmenu"}
+              className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Our Menu
-            </li>
-            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/recipes"}
+              className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Recipes
-            </li>
-            <li className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer">
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to={"/contactus"}
+              className="text-lg text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer"
+            >
               Contact Us
-            </li>
+            </Nav.Link>
           </motion.ul>
 
           <div
@@ -114,7 +131,7 @@ const Header = () => {
                 {user && user.email === "jeetgadhiya16@gmail.com" && (
                   <Link to={"/createItem"}>
                     <p
-                      className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                       onClick={() => setIsMenu(false)}
                     >
                       New Item <MdAdd />
@@ -124,24 +141,23 @@ const Header = () => {
                 {user && user.email === "20dcs021@charusat.edu.in" && (
                   <Link to={"/createItem"}>
                     <p
-                      className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                       onClick={() => setIsMenu(false)}
                     >
                       New Item <MdAdd />
                     </p>
                   </Link>
                 )}
-                {user && user.email === "shrutikodinaria@gmail.com  " && (
+                {user && user.email === "shrutikodinaria@gmail.com" && (
                   <Link to={"/createItem"}>
                     <p
-                      className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base'
+                      className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                       onClick={() => setIsMenu(false)}
                     >
                       New Item <MdAdd />
                     </p>
                   </Link>
                 )}
-
                 <p
                   className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
                   onClick={logout}
@@ -151,11 +167,12 @@ const Header = () => {
               </motion.div>
             )}
           </div>
-        </div>
+        </Nav>
+        <Outlet />
       </div>
 
       {/* mobile */}
-      <div className="flex items-center justify-between md:hidden w-full h-full ">
+      <Navbar className="flex items-center justify-between md:hidden w-full h-full ">
         <div
           className="relative flex items-center justify-center"
           onClick={showCart}
@@ -170,10 +187,10 @@ const Header = () => {
           )}
         </div>
 
-        <Link to={"/"} className="flex items-center gap-2">
+        <Navbar.Brand to={"/"} className="flex items-center gap-2">
           <img src={Logo1} className="w-8 object-cover" alt="logo1" />
           <img src={Logo} className="w-22 object-cover" alt="logo" />
-        </Link>
+        </Navbar.Brand>
 
         <div className="relative">
           <motion.img
@@ -198,32 +215,40 @@ const Header = () => {
                 </Link>
               )}
 
-              <ul className="flex flex-col ">
-                <li
+              <Nav className="flex flex-col ">
+                <Nav.Link
+                  as={Link}
+                  to={"/"}
                   className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
                   onClick={() => setIsMenu(false)}
                 >
                   Home
-                </li>
-                <li
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to={"/ourmenu"}
                   className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
                   onClick={() => setIsMenu(false)}
                 >
-                  Menu
-                </li>
-                <li
+                  Our Menu
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to={"/recipes"}
                   className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
                   onClick={() => setIsMenu(false)}
                 >
-                  About Us
-                </li>
-                <li
+                  Recipes
+                </Nav.Link>
+                <Nav.Link
+                  as={Link}
+                  to={"/contactus"}
                   className="text-base text-textColor hover:text-headingColor duration-100 transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2"
                   onClick={() => setIsMenu(false)}
                 >
-                  Service
-                </li>
-              </ul>
+                  Contact Us
+                </Nav.Link>
+              </Nav>
 
               <p
                 className="m-2 p-2 rounded-md shadow-md flex items-center justify-center bg-gray-200 gap-3 cursor-pointer hover:bg-gray-300 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -234,8 +259,9 @@ const Header = () => {
             </motion.div>
           )}
         </div>
-      </div>
-    </header>
+      </Navbar>
+      <Outlet />
+    </Navbar>
   );
 };
 
