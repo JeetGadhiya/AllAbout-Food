e20e0222f80cc914e1f93dbc68d0b637871bb0cd
Jeet Gadhiya <88260132+JeetGadhiya@users.noreply.github.com>
2022-09-24 12:39:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { actionType } from "./context/reducer";
 import Recipes from "./components/Recipes/Recipes";
 import ContactUs from "./components/ContactUs/ContactUs";
 import OurMenu from "./components/OurMenu/OurMenu";
+import './App.css';
 
 const App = () => {
   const [{ foodItems }, dispatch] = useStateValue();
@@ -27,18 +28,18 @@ const App = () => {
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <div className="w-screen h-auto flex flex-col bg-primary">
+      <div className='w-screen h-auto flex flex-col bg-primary'>
         <Header />
 
-        <main className="mt-14 md:mt-20 px-4 md:px-16 py-4 w-full">
+       
           <Routes>
-            <Route path="/*" element={<MainContainer />} />
-            <Route path="/createItem" element={<CreateContainer />} />
-            <Route path="/ourmenu" element={<OurMenu />} />
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/contactus" element={<ContactUs />} />
+            <Route path='/*' element={<MainContainer />} />
+            <Route path='/createItem' element={<CreateContainer />} />
+            <Route path='/ourmenu' element={<OurMenu />} />
+            <Route path='/recipes' element={<Recipes />} />
+            <Route path='/contactus' element={<ContactUs />} />
           </Routes>
-        </main>
+        
       </div>
     </AnimatePresence>
   );
